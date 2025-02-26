e79311607d3a72802060e23720c4d98e81fb2d46
Cheonho <ghtp8571@naver.com>
2025-02-26 17:21:14 +0900

    refactor: 게시판 boardNum entity(long) -> dto(string) 으로 변경

--- src/main/java/com/bno/board_back/dto/object/BoardListView.java
+++ src/main/java/com/bno/board_back/dto/object/BoardListView.java
@@ -11,7 +11,7 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class BoardListView {
 
-    private Long boardNum;
+    private String boardNum;
     private String title;
     private String content;
     private String writerEmail;
--- src/main/java/com/bno/board_back/mapper/BoardListViewMapper.java
+++ src/main/java/com/bno/board_back/mapper/BoardListViewMapper.java
@@ -3,9 +3,20 @@ package com.bno.board_back.mapper;
 import com.bno.board_back.dto.object.BoardListView;
 import com.bno.board_back.entity.BoardListViewEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface BoardListViewMapper extends GenericMapper<BoardListView, BoardListViewEntity> {
+
+    @Named("mapLongToString")
+    default String mapLongToString(Long value) {
+        return value != null ? String.valueOf(value) : null;
+    }
+
+    @Mapping(target = "boardNum", source = "boardNum", qualifiedByName = "mapLongToString")
+    BoardListView toDTO(BoardListViewEntity boardListViewEntity);
+
     BoardListViewMapper INSTANCE = Mappers.getMapper(BoardListViewMapper.class);
 }

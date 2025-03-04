7b936818d20e102239e0f81e848cae725f27ff9c
Cheonho <ghtp8571@naver.com>
2025-03-04 11:11:22 +0900

    refactor: page 정보 header로 넘기도록 수정

--- src/main/java/com/bno/board_back/config/WebConfig.java
+++ src/main/java/com/bno/board_back/config/WebConfig.java
@@ -12,6 +12,8 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")  // 모든 요청에 대해 CORS 허용
                 .allowedOrigins("*")  // React 앱에서만 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")  // 허용할 HTTP 메소드
-                .allowedHeaders("*");  // 허용할 헤더
+                .allowedHeaders("*")  // 허용할 헤더
+                .exposedHeaders("X-Total-Page", "X-Total-Elements", "X-Page-Number", "X-Page-Size",
+                        "X-Current-Section", "X-First-Page-Number", "X-Last-Page-Number");
     }
 }
--- src/main/java/com/bno/board_back/dto/response/board/GetBoardListResponseDto.java
+++ src/main/java/com/bno/board_back/dto/response/board/GetBoardListResponseDto.java
@@ -1,12 +1,16 @@
 package com.bno.board_back.dto.response.board;
 
+import com.bno.board_back.common.ResponseCode;
+import com.bno.board_back.common.ResponseMessage;
 import com.bno.board_back.dto.object.BoardListView;
 import com.bno.board_back.dto.object.PageDto;
+import com.bno.board_back.dto.response.ResponseDto;
 import com.bno.board_back.entity.BoardListViewEntity;
 import com.bno.board_back.mapper.BoardListViewMapper;
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -14,17 +18,27 @@ import java.util.List;
 
 @Getter
 @ToString
-public class GetBoardListResponseDto extends PageDto {
+public class GetBoardListResponseDto extends ResponseDto {
 
     private final List<BoardListView> boardList;
+    private static PageDto pageDto = null;
 
     private GetBoardListResponseDto(Page<BoardListViewEntity> page) {
-        super(page.getTotalPages(), page.getTotalElements(), page.getNumber(), page.getSize());
+        super(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
         this.boardList = BoardListViewMapper.INSTANCE.toDtoList(page.getContent());
     }
 
     public static ResponseEntity<GetBoardListResponseDto> sucess(Page<BoardListViewEntity> page) {
         GetBoardListResponseDto result = new GetBoardListResponseDto(page);
-        return ResponseEntity.status(HttpStatus.OK).body(result);
+        pageDto = new PageDto(page.getTotalPages(), page.getTotalElements(), page.getNumber(), page.getSize());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Total-Elements", String.valueOf(pageDto.getTotalElements()));
+        headers.add("X-Total-Page", String.valueOf(pageDto.getTotalPage()));
+        headers.add("X-Page-Number", String.valueOf(pageDto.getPageNumber()));
+        headers.add("X-Page-Size", String.valueOf(pageDto.getPageSize()));
+        headers.add("X-Current-Section", String.valueOf(pageDto.getCurrentSection()));
+        headers.add("X-First-Page-Number", String.valueOf(pageDto.getFirstPageNumber()));
+        headers.add("X-Last-Page-Number", String.valueOf(pageDto.getLastPageNumber()));
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(result);
     }
 }

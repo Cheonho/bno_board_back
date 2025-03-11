a079eaf30d188e08b2a78377ee4b5f868059bceb
Cheonho <ghtp8571@naver.com>
2025-03-11 15:36:01 +0900

    feat: swagger multipart/form-data json 받을 수 있도록 설정 추가

---
+++ src/main/java/com/bno/board_back/config/MultipartJackson2HttpMessageConverter.java
@@ -0,0 +1,32 @@
+package com.bno.board_back.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Type;
+
+// swagger multipart/form-data request에서 json 받을 수 있도록 설정
+@Component
+public class MultipartJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+
+    public MultipartJackson2HttpMessageConverter(ObjectMapper objectMapper) {
+        super(objectMapper, MediaType.APPLICATION_OCTET_STREAM);
+    }
+
+    @Override
+    public boolean canWrite(Class<?> clazz, MediaType mediaType) {
+        return false;
+    }
+
+    @Override
+    public boolean canWrite(Type type, Class<?> clazz, MediaType mediaType) {
+        return false;
+    }
+
+    @Override
+    protected boolean canWrite(MediaType mediaType) {
+        return false;
+    }
+}

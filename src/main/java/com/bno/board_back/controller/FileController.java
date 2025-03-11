d5e8e6bcc4c1faa2225cffaea275aadfc9bbe2cd
Cheonho <ghtp8571@naver.com>
2025-03-11 16:16:43 +0900

    fix: 파일 upload url로 접근 시 boardNum param으로 가져오도록 수정

--- src/main/java/com/bno/board_back/controller/FileController.java
+++ src/main/java/com/bno/board_back/controller/FileController.java
@@ -5,10 +5,7 @@ import com.bno.board_back.service.FileService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -27,7 +24,7 @@ public class FileController {
             produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ResponseDto> uploadFile(
             @RequestPart("file") MultipartFile file,
-            @RequestPart("boardNum") String boardNum
+            @RequestParam("boardNum") String boardNum
     ) {
         String test = fileService.fileUpload(file, boardNum);
         return null;

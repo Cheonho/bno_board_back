6c278cf021acf5be5556db18df86a2740e1c951c
Cheonho <ghtp8571@naver.com>
2025-03-11 15:35:24 +0900

    fix: 빌드 시 경고 문구 수정

--- src/main/java/com/bno/board_back/mapper/BoardWriteMapper.java
+++ src/main/java/com/bno/board_back/mapper/BoardWriteMapper.java
@@ -5,14 +5,20 @@ import com.bno.board_back.entity.BoardEntity;
 import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
-//@Mapper(componentModel = "spring", config = GenericMapper.class)
 @Mapper(componentModel = "spring")
 public interface BoardWriteMapper extends GenericMapper<WriteBoards, BoardEntity> {
 
     @Mapping(target = "createAt", expression = "java(java.time.LocalDateTime.now())")
+    @Mapping(target = "updateAt", expression = "java((java.time.LocalDateTime) null)")
     @Mapping(target = "viewCount", expression = "java(0)")
     @Mapping(target = "status", expression="java(true)")
     BoardEntity toEntity(WriteBoards writeBoards);
+
+    @Mapping(target = "createAt", ignore = true)
+    @Mapping(target = "updateAt", ignore = true)
+    @Mapping(target = "viewCount", ignore = true)
+    @Mapping(target = "status", ignore = true)
+    void updateFormDto(WriteBoards dto, @MappingTarget BoardEntity entity);
 
     BoardWriteMapper INSTANCE = Mappers.getMapper(BoardWriteMapper.class);
 }

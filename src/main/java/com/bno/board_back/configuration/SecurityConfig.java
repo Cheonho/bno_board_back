683cf05de8bbdc229ca3543e7ccb0373cdd4b846
Cheonho <ghtp8571@naver.com>
2025-03-12 10:38:44 +0900

    fix: securityConfig에 filter추가

--- src/main/java/com/bno/board_back/configuration/SecurityConfig.java
+++ src/main/java/com/bno/board_back/configuration/SecurityConfig.java
@@ -1,35 +1,43 @@
 package com.bno.board_back.configuration;
 
+import com.bno.board_back.filter.JwtAuthenticationFilter;
+import com.bno.board_back.provider.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // CSRF 비활성화
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Stateless 세션 설정
-
+//                .httpBasic(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/login/**", "/join",
                                 "/idcheck/**", "/namecheck/**" ,
                                 "/nicknamecorrection/**", "/addresscorrection/**",
-                                "/passwordcorrection/**","/board/board-list",
+                                "/passwordcorrection/**",
                                 "/mypage/**",
                                 "/search-list/**", "detailBoard",
                                 "*/view", "/swagger-ui/**", "/v3/api-docs/**",
-                                "/**", "/*"
+                                "/board/board-list"
+//                                "/**", "/*"
                         ).permitAll() // 특정 URL에 대한 접근 허용
                         .anyRequest().authenticated() // 나머지 요청은 인증 필요
-                );
+                )
+
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
 
         return http.build();
